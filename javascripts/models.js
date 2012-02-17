4b17ba5846900b521899f8a9c58df3fd5aa7818f
Cat Chen <cathsfz@gmail.com>
2012-02-17 22:33:31 +0800

    working copy

--- javascripts/models.js
+++ javascripts/models.js
@@ -2,8 +2,8 @@ var File = Backbone.Model.extend({
     
 });
 
-var Directory = Backbone.Collection.extend({
+var FileList = Backbone.Collection.extend({
     model: File
 });
 
-var root = new Directory();
+var root = new FileList();
